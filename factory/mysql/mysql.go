bbc58b740a8a049d49114f88dff46b4177bd8182
魔人舅舅 <kmlixh@foxmail.com>
2016-11-27 16:47:55 +0800

    test all

--- factory/mysql/mysql.go
+++ factory/mysql/mysql.go
@@ -1,13 +1,25 @@
 package mysql
 
 import "github.com/gom"
+import _ "github.com/go-sql-driver/mysql"
 
-
+func init()  {
+	gom.Register("mysql",&MySqlFactory{})
+}
 
 type MySqlFactory struct {
 
 }
 
 func Insert(model gom.TableModel) (string,[]interface{}) {
+
+}
+func Delete(model gom.TableModel) (string,[]interface{}) {
+
+}
+func Update(model gom.TableModel) (string,[]interface{}) {
+
+}
+func Query(model gom.TableModel) (string,[]interface{}) {
 
 }
