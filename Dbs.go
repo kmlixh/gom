6e42386a154a434a4a4c8161f5e3ce653138ca0d
魔人老爸 <kmlixh@foxmail.com>
2017-05-14 00:20:33 +0800

    修复代码bug，尝试创建测试范例

--- Dbs.go
+++ Dbs.go
@@ -2,178 +2,172 @@ package gom
 
 import (
 	"database/sql"
-	"reflect"
 	"fmt"
+	"reflect"
 )
 
 type DB struct {
-	factory SqlFactory
-	db      * sql.DB
+	Factory SqlFactory
+	Db      *sql.DB
 }
-type Execute func(TableModel)(string,[]interface{})
+type Execute func(TableModel) (string, []interface{})
 
 type Executor struct {
 	execute Execute
-	tms []TableModel
+	tms     []TableModel
 }
 
 type ExecutorType int
 
 const (
-	_ ExecutorType =iota
+	_ ExecutorType = iota
 	Insert
 	Delete
 	Update
 )
 
-func GetExecutor(executorType ExecutorType,tableModel []TableModel) Executor {
+func (DB DB) GetExecutor(executorType ExecutorType, tableModel []TableModel) Executor {
 	var execute Execute
 	switch executorType {
 	case Insert:
-		execute=DB.factory.Insert
+		execute = DB.Factory.Insert
 	case Delete:
-		execute=DB.factory.Delete
+		execute = DB.Factory.Delete
 	case Update:
-		execute=DB.factory.Update
+		execute = DB.Factory.Update
 	}
-	return Executor{execute,tableModel}
+	return Executor{execute, tableModel}
 }
 
-func (DB DB) exec(executor Executor)(int,error){
+func (DB DB) exec(executor Executor) (int, error) {
 	var results int
-	for _,model:=range executor.tms{
-		sqls,datas:=executor.execute(model)
+	for _, model := range executor.tms {
+		sqls, datas := executor.execute(model)
 		if debug {
-			fmt.Println(sqls,datas)
+			fmt.Println(sqls, datas)
 		}
-		result,err:=DB.db.Exec(sqls,datas...)
-		if(err!=nil){
-			return results,err
-		}else{
-			rows,_:=result.RowsAffected()
-			results+=int(rows)
+		result, err := DB.Db.Exec(sqls, datas...)
+		if err != nil {
+			return results, err
+		} else {
+			rows, _ := result.RowsAffected()
+			results += int(rows)
 		}
 	}
-	return results,nil
+	return results, nil
 }
-func (DB DB) execTransc(executors...Executor)(int,error){
-	result:=0
-	tx,err:=DB.db.Begin()
-	if err!=nil{
-		return result,err
+func (DB DB) execTransc(executors ...Executor) (int, error) {
+	result := 0
+	tx, err := DB.Db.Begin()
+	if err != nil {
+		return result, err
 	}
-	for _,executor:=range executors{
-		result,err=DB.exec(executor)
-		if err!=nil {
+	for _, executor := range executors {
+		result, err = DB.exec(executor)
+		if err != nil {
 			tx.Rollback()
-			return 0,err
+			return 0, err
 		}
 	}
 	tx.Commit()
-	return result,nil;
+	return result, nil
 }
-func (DB DB) Insert(vs...interface{})(int,error){
-	models:= getTableModels(vs...)
-	return DB.exec(Executor{DB.factory.Insert, models})
+func (DB DB) Insert(vs ...interface{}) (int, error) {
+	models := getTableModels(vs...)
+	return DB.exec(Executor{DB.Factory.Insert, models})
 }
-func (DB DB) InsertInTransaction(vs...interface{})(int,error){
-	tables:= getTableModels(vs...)
-	return DB.execTransc(DB.factory.Insert,tables...)
+func (DB DB) InsertInTransaction(vs ...interface{}) (int, error) {
+	tables := getTableModels(vs...)
+	return DB.execTransc(Executor{DB.Factory.Insert, tables})
 }
-func (DB DB) Delete(vs...interface{})(int,error){
-	tables:= getTableModels(vs...)
-	return DB.exec(Executor{DB.factory.Delete,tables})
+func (DB DB) Delete(vs ...interface{}) (int, error) {
+	tables := getTableModels(vs...)
+	return DB.exec(Executor{DB.Factory.Delete, tables})
 }
-func (DB DB) DeleteInTransaction(vs...interface{})(int,error){
-	tables:= getTableModels(vs...)
-	return DB.execTransc(DB.factory.Delete,tables...)
+func (DB DB) DeleteInTransaction(vs ...interface{}) (int, error) {
+	tables := getTableModels(vs...)
+	return DB.execTransc(Executor{DB.Factory.Delete, tables})
 }
-func (DB DB) DeleteByConditon(v interface{},c Condition)(int,error){
-	tableModel:= getTableModel(v)
-	tableModel.Cnd=c
-	return DB.exec(Executor{DB.factory.Delete,tableModel})
+func (DB DB) DeleteByConditon(v interface{}, c Condition) (int, error) {
+	tableModel := getTableModel(v)
+	tableModel.Cnd = c
+	return DB.exec(Executor{DB.Factory.Delete, []TableModel{tableModel}})
 }
-func (DB DB) DeleteByConditonInTransaction(v interface{},c Condition)(int,error){
-	tableModel:= getTableModel(v)
-	tableModel.Cnd=c
-	return DB.execTransc(DB.factory.Delete,tableModel)
+func (DB DB) DeleteByConditonInTransaction(v interface{}, c Condition) (int, error) {
+	tableModel := getTableModel(v)
+	tableModel.Cnd = c
+	return DB.execTransc(Executor{DB.Factory.Delete, []TableModel{tableModel}})
 }
-func (DB DB) Update(vs...interface{})(int,error){
+func (DB DB) Update(vs ...interface{}) (int, error) {
 	tms := getTableModels(vs...)
-	return DB.exec(Executor{DB.factory.Update, tms})
+	return DB.exec(Executor{DB.Factory.Update, tms})
 }
-func (DB DB) UpdateInTransaction(vs...interface{})(int,error) {
-	tables:= getTableModels(vs...)
-	return DB.execTransc(DB.factory.Update,tables...)
+func (DB DB) UpdateInTransaction(vs ...interface{}) (int, error) {
+	tables := getTableModels(vs...)
+	return DB.execTransc(Executor{DB.Factory.Update, tables})
 }
-func (DB DB) UpdateByCondition(v interface{},c Condition)(int,error){
-	tableModel:= getTableModel(v)
-	tableModel.Cnd=c
-	return DB.exec(Executor{DB.factory.Update,tableModel})
+func (DB DB) UpdateByCondition(v interface{}, c Condition) (int, error) {
+	tableModel := getTableModel(v)
+	tableModel.Cnd = c
+	return DB.exec(Executor{DB.Factory.Update, []TableModel{tableModel}})
 }
-func (DB DB) UpdateByConditionInTransaction(v interface{},c Condition)(int,error){
-	tableModel:= getTableModel(v)
-	tableModel.Cnd=c
-	return DB.exec(Executor{DB.factory.Update,tableModel})
+func (DB DB) UpdateByConditionInTransaction(v interface{}, c Condition) (int, error) {
+	tableModel := getTableModel(v)
+	tableModel.Cnd = c
+	return DB.exec(Executor{DB.Factory.Update, []TableModel{tableModel}})
 }
 
-func (DB DB) Query(vs interface{},c Condition) interface{}{
-	tps,isPtr,islice:= getType(vs)
-	model:= getTableModel(vs)
-	if debug{
-		fmt.Println("model:",model)
+func (DB DB) Query(vs interface{}, c Condition) interface{} {
+	tps, isPtr, islice := getType(vs)
+	model := getTableModel(vs)
+	if debug {
+		fmt.Println("model:", model)
 	}
-	if len(model.TableName)>0{
-		model.Cnd=c;
-		if islice{
+	if len(model.TableName) > 0 {
+		model.Cnd = c
+		if islice {
 			results := reflect.Indirect(reflect.ValueOf(vs))
-			sqls,adds:=DB.factory.Query(model)
+			sqls, adds := DB.Factory.Query(model)
 			if debug {
-				fmt.Println(sqls,adds)
+				fmt.Println(sqls, adds)
 			}
-			rows,err:=DB.db.Query(sqls,adds...)
-			if err!=nil{
+			rows, err := DB.Db.Query(sqls, adds...)
+			if err != nil {
 				return nil
 			}
 			defer rows.Close()
 			for rows.Next() {
-				val:=getValueOfTableRow(model,rows)
+				val := getValueOfTableRow(model, rows)
 				if isPtr {
-					results.Set(reflect.Append(results,val.Elem()))
-				}else{
-					results.Set(reflect.Append(results,val))
+					results.Set(reflect.Append(results, val.Elem()))
+				} else {
+					results.Set(reflect.Append(results, val))
 				}
 			}
 			return vs
 
-		}else {
-			sqls,adds:=DB.factory.Query(model)
+		} else {
+			sqls, adds := DB.Factory.Query(model)
 			if debug {
-				fmt.Println(sqls,adds)
+				fmt.Println(sqls, adds)
 			}
-			row:=DB.db.QueryRow(sqls,adds...)
-			if debug{
-				fmt.Println("row is",row)
+			row := DB.Db.QueryRow(sqls, adds...)
+			if debug {
+				fmt.Println("row is", row)
 			}
-			val:=getValueOfTableRow(model,row)
+			val := getValueOfTableRow(model, row)
 			var vt reflect.Value
-			if(isPtr){
-				vt=reflect.ValueOf(vs).Elem()
-			}else{
-				vt=reflect.New(tps).Elem()
+			if isPtr {
+				vt = reflect.ValueOf(vs).Elem()
+			} else {
+				vt = reflect.New(tps).Elem()
 
 			}
 			vt.Set(val.Elem())
 			return vt.Interface()
 		}
 
-	}else{
+	} else {
 		return nil
 	}
 }
-
-
-
-
-
-
--- model_test.go
+++ model_test.go
@@ -1,27 +1,26 @@
 package gom
 
 import (
-	"time"
 	"testing"
-	_ "github.com/gom/factory/mysql"
+	"time"
 )
+
 type Log struct {
-	Id string `json:"id" gom:"!"`
-	Level int `gom:"level"`
-	Info string `gom:"info"`
-	Test string
-	Date time.Time `gom:"#"`
+	Id    string `json:"id" gom:"!"`
+	Level int    `gom:"level"`
+	Info  string `gom:"info"`
+	Test  string
+	Date  time.Time `gom:"#"`
 }
 type User struct {
-	Id int `json:"id" gom:"@,id"`
-	SessionId string `json:"session_id" gom:"-"`
-	Pwd string `json:"pwd" gom:"pwd"`
-	Email string `json:"email" gom:"email"`
-	Valid int `json:"valid" gom:"valid"`
-	NickName string `json:"nicks" gom:"nick_name"`
-	RegDate time.Time `json:"reg_date" gom:"reg_date"`
+	Id        int       `json:"id" gom:"@,id"`
+	SessionId string    `json:"session_id" gom:"-"`
+	Pwd       string    `json:"pwd" gom:"pwd"`
+	Email     string    `json:"email" gom:"email"`
+	Valid     int       `json:"valid" gom:"valid"`
+	NickName  string    `json:"nicks" gom:"nick_name"`
+	RegDate   time.Time `json:"reg_date" gom:"reg_date"`
 }
-
 
 func (User) TableName() string {
 	return "user"
@@ -30,10 +29,9 @@ func (Log) TableName() string {
 	return "system_log"
 }
 
-
 func TestModel(t *testing.T) {
 	var log Log
-	m1:=getTableModel(&log)
-	m2:=getTableModel(log)
-	t.Log(m1,m2)
+	m1 := getTableModel(&log)
+	m2 := getTableModel(log)
+	t.Log(m1, m2)
 }
