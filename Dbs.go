230f32b1f8582c3c915da92b4f63047ff8eebda6
魔人 <kmlixh@foxmail.com>
2017-05-15 10:29:49 +0800

    修改方法名称

--- Dbs.go
+++ Dbs.go
@@ -83,7 +83,7 @@ func (db DB) Insert(vs ...interface{}) (int, error) {
 }
 func (db DB) InsertInTransaction(vs ...interface{}) (int, error) {
 	tables := getTableModels(vs...)
-	return db.DoExecutorInTransaction(TransactionJob{db.Factory.Insert, tables})
+	return db.WorkInTransaction(TransactionJob{db.Factory.Insert, tables})
 }
 func (db DB) Delete(vs ...interface{}) (int, error) {
 	tables := getTableModels(vs...)
@@ -91,7 +91,7 @@ func (db DB) Delete(vs ...interface{}) (int, error) {
 }
 func (db DB) DeleteInTransaction(vs ...interface{}) (int, error) {
 	tables := getTableModels(vs...)
-	return db.DoExecutorInTransaction(TransactionJob{db.Factory.Delete, tables})
+	return db.WorkInTransaction(TransactionJob{db.Factory.Delete, tables})
 }
 func (db DB) DeleteByConditon(v interface{}, c Condition) (int, error) {
 	tableModel := getTableModel(v)
@@ -101,7 +101,7 @@ func (db DB) DeleteByConditon(v interface{}, c Condition) (int, error) {
 func (db DB) DeleteByConditonInTransaction(v interface{}, c Condition) (int, error) {
 	tableModel := getTableModel(v)
 	tableModel.Cnd = c
-	return db.DoExecutorInTransaction(TransactionJob{db.Factory.Delete, []TableModel{tableModel}})
+	return db.WorkInTransaction(TransactionJob{db.Factory.Delete, []TableModel{tableModel}})
 }
 func (db DB) Update(vs ...interface{}) (int, error) {
 	tms := getTableModels(vs...)
@@ -109,7 +109,7 @@ func (db DB) Update(vs ...interface{}) (int, error) {
 }
 func (db DB) UpdateInTransaction(vs ...interface{}) (int, error) {
 	tables := getTableModels(vs...)
-	return db.DoExecutorInTransaction(TransactionJob{db.Factory.Update, tables})
+	return db.WorkInTransaction(TransactionJob{db.Factory.Update, tables})
 }
 func (db DB) UpdateByCondition(v interface{}, c Condition) (int, error) {
 	tableModel := getTableModel(v)
