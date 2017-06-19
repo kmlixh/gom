490f5878863e487246cd1adacdc233af6876a82c
魔人 <kmlixh@foxmail.com>
2017-06-19 17:32:20 +0800

    优化项目结构

--- factory/mysql/mysql.go
+++ factory/mysql/mysql.go
@@ -21,7 +21,7 @@ func (MySqlFactory) Insert(model gom.TableModel) (string, []interface{}) {
 	for i, v := range ccs {
 		value := model.ModelValue.FieldByName(v.FieldName).Interface()
 		if (!v.Auto) && value != nil {
-			if i > 0 {
+			if i > 1 {
 				sql += ","
 				values += ","
 			}
@@ -58,7 +58,7 @@ func (MySqlFactory) Update(model gom.TableModel) (string, []interface{}) {
 	for i, v := range model.Columns {
 		value := model.ModelValue.FieldByName(v.FieldName).Interface()
 		if (!v.Auto) && value != nil {
-			if i > 0 {
+			if i > 1 {
 				sql += ","
 			}
 			sql += "`" + v.ColumnName + "` = ? "
