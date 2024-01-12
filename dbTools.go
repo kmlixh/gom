4e443b00d05d177429dda4c16f1f94d6ada772dd
李兴华 <kmlixh@foxmail.com>
2024-01-12 17:16:17 +0800

    增加自动生成结构体的方法

---
+++ dbTools.go
@@ -0,0 +1,9 @@
+package gom
+
+type DBTools struct {
+}
+
+func (t DBTools) GenTableModel(db *DB, path string, outDir string, packageName string, fileName string, tables ...string) error {
+
+	return nil
+}
